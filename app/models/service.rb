3ee25e9bdb6f853b10067de9e6a8c722ce26e8b6
Nima Nowroozi <nowrouzi.nima@gmail.com>
2015-06-12 16:33:23 +0430

    work on many to many

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ActiveRecord::Base
   belongs_to :service_category
   belongs_to :user
-  has_and_belongs_to_many :tags
+  has_many :tags, :through => :services_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,3 @@
 class Tag < ActiveRecord::Base
-  has_and_belongs_to_many :services
+  has_many :services, :through => :services_tags
 end
